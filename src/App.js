d55899996bd71b142ce2aac1d16c12d3de7d0c79
whisperworker <thewhisperworker@gmail.com>
2021-09-12 20:15:33 +0300

    make decomposition

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "Python", body: 'Python - Язык программирования'},
-        {id: 3, title: "JS Hooks", body: 'JavaScript - Язык программирования'},
-        {id: 2, title: "Ajax principles", body: 'Ajax - принцип...'},
-    ])
-    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
 
-    /*Отрабатывает только тогда когда удаляем,
-    добавляем, либо меняем алгоритм сортировки*/
-    const sortedPosts = useMemo(() => {
-        console.log('123')
-        /*Валидация выбранного элемента*/
-        if (filter.sort) {
-            /*Мутируем копию массива
-            а не мутируем состояние на прямую*/
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
-    /*Поиск и сортировка*/
-    const sortedAndSearchedPosts = useMemo(() => {
-        /*Фильтруем посты по заданному SearchQuery в инпуте*/
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
-    
     /*Функция на вход ожидает созданный post
     который создается в компоненте postForm*/
     const createPost = (newPost) => {
         /*Изменяем состояние добавлением
         newPost в конец массива постов*/
         setPosts([...posts, newPost])
+        /*При создании поста скрываем модальное окно*/
+        setModal(false)
     }
 
     /*Получаем post из дочернего элемента*/
@@ -49,24 +44,29 @@ function App() {
 
     return (
         <div className="App">
-            {/*Передаем в компонент PostForm функцию обратного вызова*/}
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать пост
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                {/*Передаем в компонент PostForm функцию обратного вызова*/}
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter
                 filter={filter}
                 setFilter={setFilter}
             />
-            {/*Условная отрисовка*/}
-            {sortedAndSearchedPosts.length !== 0 ?
-                /*Передаем в PostList функцию обратного вызова
-                Так же в компонент PostList передаем все посты
-                и заголовок для отображения на странице*/
-                <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
-                :
-                <h1 style={{textAlign: 'center'}}>
-                    Посты не были найдены!
-                </h1>
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
             }
+            {isPostsLoading
+                ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader/></div>
+                    /*Передаем в PostList функцию обратного вызова
+                    Так же в компонент PostList передаем все посты
+                    и заголовок для отображения на странице*/
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+            }
+
         </div>
     )
 }
