64c05a5c9f6f621478eb4772cde535e2d670040d
whisperworker <thewhisperworker@gmail.com>
2021-09-12 20:24:20 +0300

    add animation for add, delete post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 //PostList принимает в себя посты и заголовок
 const PostList = ({posts, title, remove}) => {
+
+    /*Условная отрисовка*/
+    if (!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                Посты не были найдены!
+            </h1>
+        )
+    }
     return (
         <div>
             {/*Отображаем заголовок внутри App*/}
@@ -10,12 +20,20 @@ const PostList = ({posts, title, remove}) => {
                 {title}
             </h1>
 
-            {/*Создаем новый массив на основе постов*/}
-            {posts.map((post, index) =>
-                /*Передаем функцию remove из App в PostItem*/
-                /*Передаем каждый пост в PostItem*/
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )}
+            <TransitionGroup>
+                {/*Создаем новый массив на основе постов*/}
+                {posts.map((post, index) =>
+                    /*Передаем функцию remove из App в PostItem*/
+                    /*Передаем каждый пост в PostItem*/
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames={"post"}
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
